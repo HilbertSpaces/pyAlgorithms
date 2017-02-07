4eb4a7716cc829f231f05e4dea93b80b15104348
HilbertSpaces <Jktecon@gmail.com>
2017-02-07 15:40:38 -0500

    quickSort

--- SortSearch/quickSort.py
+++ SortSearch/quickSort.py
@@ -1,34 +1,27 @@
 import numpy as np
-global B
-B=0
 def partition(A,l,r):
-    global B
     p=A[l]
     i=l+1
-    B+=len(A)-1
-    print(B)
+    print(A)
     for j in range(l+1,r):
         if A[j]<p:
-            A[i],A[j]=A[j],A[i]
+            save=A[i]
+            A[i]=A[j]
+            A[j]=save
             i+=1
-    A[i-1],A[l]=A[l],A[i-1]
-    return B,i-1
+    save=A[i-1]
+    A[i-1]=A[l]
+    A[l]=save
+    return i-1
 def quickSort(A,n):
-    global B
     if n>1:
-        part=partition(A,0,n)
-        split=part[1]
-        B=part[0]
+        split=partition(A,0,n)
         M=A[:split]
         N=A[split+1:]
         quickSort(M,len(M))
         quickSort(N,len(N))
-    return B
+        print(A)
+    return A
 
-with open('stanfordArray.txt') as f:
-    content=f.readlines()
-A=([int(x.strip()) for x in content])
-R=np.array(A)
-total=quickSort(R,len(R))
-A=R.tolist()
-print(B)
+A=np.array([3,4,5,2,10,7,6,9,1])
+print(quickSort(A,len(A)))
